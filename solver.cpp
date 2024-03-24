54073b0c6b93a754a97b761226198867f20ca2b8
Dm1tryb1t <afanasevdima465@gmail.com>
2024-03-24 11:28:48 +0300

    nn

--- solver.cpp
+++ solver.cpp
@@ -5,20 +5,28 @@ Solver::Solver() {}
 QString Solver::getAnswer(const QString& language, const QString& filename) {
     QProcess process;
     // process.open();
+    process.setWorkingDirectory("C:/");
+    process.setProcessChannelMode(QProcess::ProcessChannelMode::MergedChannels);
     if (language.contains("++")) {
         // process.startCommand("g++ " + filename + " -o C:\\prog -w");
-        process.start("C:\\Windows\\System32\\cmd.exe", QStringList() << "g++ " + filename + " -o C:\\prog -w");
-        process.waitForReadyRead();
-        QString error = QString(process.readAllStandardOutput());
+        QString progname = filename;
+        if (progname.endsWith(".cc")) {
+            progname.resize(progname.size() - 3);
+        } else {
+            progname.resize(progname.size() - 4);
+        }
+        process.start("cmd.exe", QStringList() << "g++" << filename << "-o" << progname);
+        process.waitForFinished();
+        QString error = QString(process.readAll());
         if (error != "") {
             process.close();
             return error + "-2";
         }
         // process.startCommand("C:\\prog");
-        process.start("C:\\Windows\\System32\\cmd.exe", QStringList() <<  "C:\\prog");
-        process.waitForReadyRead();
-        QString answer = QString(process.readAllStandardOutput());
-        process.startCommand("rm C:\\prog");
+        process.start("cmd.exe", QStringList() << progname);
+        process.waitForFinished();
+        QString answer = QString(process.readAll());
+        // process.startCommand("rm C:\\prog");
         process.close();
         return answer + "-3";
     } /*else if (language.contains("thon")) {
